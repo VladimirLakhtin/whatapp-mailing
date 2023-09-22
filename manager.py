f5d8235294ee3f9017685ea58f5ea7adbb8e9694
VladimirLakhtin <vllakhtin@gmail.com>
2023-09-22 20:49:21 +0300

    fix: delete unusable method

--- manager.py
+++ manager.py
@@ -26,30 +26,6 @@ class WhatsAppManager:
         core.close_tab(wait_time=close_time)
         time.sleep(random.randint(5, 15))
 
-    def send_image(self, path: str, caption: str, receiver: str, wait_time: int) -> None:
-        """Sends the Image to a Contact or a Group based on the Receiver"""
-
-        _web(message=caption, receiver=receiver)
-
-        time.sleep(7)
-        time.sleep(wait_time - 7)
-        copy_image(path=path)
-        if not check_number(number=receiver):
-            for char in caption:
-                if char == "\n":
-                    pg.hotkey("shift", "enter")
-                else:
-                    pg.typewrite(char)
-        else:
-            pg.typewrite(" ")
-        if os.system().lower() == "darwin":
-            pg.hotkey("command", "v")
-        else:
-            pg.hotkey("ctrl", "v")
-        time.sleep(random.randint(10, 25))
-        pg.press("enter")
-        time.sleep(random.randint(5, 15))
-
     def sendwhats_image(self, receiver: str, img_path: str, caption: str = "", wait_time: int = 15,
                         close_time: int = 3) -> None:
         """Send Image to a WhatsApp Contact or Group at a Certain Time"""
@@ -57,7 +33,9 @@ class WhatsAppManager:
         if (not receiver.isalnum()) and (not core.check_number(number=receiver)):
             raise exceptions.CountryCodeException("Country Code Missing in Phone Number!")
 
+        time.sleep(random.randint(10, 25))
         core.send_image(
             path=img_path, caption=caption, receiver=receiver, wait_time=wait_time
         )
+        time.sleep(random.randint(5, 15))
         core.close_tab(wait_time=close_time)
